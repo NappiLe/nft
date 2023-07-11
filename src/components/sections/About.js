2d7bb05b2c9fb91e82264d7158e0df02337f5a23
NgocLe <bitbucket@example.com>
2023-07-11 16:41:38 +0700

    Done with About component

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import styled from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
+import {dark} from '../../styles/Themes'
+import Carousel from '../Carousel'
+import Button from './Button'
 
 const Section = styled.section`
   min-height: 100vh;
@@ -11,9 +14,70 @@ const Section = styled.section`
   position: relative;
 `
 
+const Container = styled.div`
+  width: 75%;
+  min-height: 80vh;
+  margin: 0 auto;
+  // background-color: lightblue;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Box = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Title= styled.h2`
+    font-size: ${props => props.theme.fontxxl};
+    text-transform: capitalize;
+    width: 80%;
+    color: ${props => props.theme.body};
+    align-self: flex-start;
+    margin: 0 auto;
+`
+const SubText= styled.p`
+    font-size: ${props => props.theme.fontlg};
+    color: ${props => props.theme.body};
+    align-self: flex-start;
+    margin: 1rem auto;
+    font-weight: 400;
+`
+
+const SubTextLight= styled.p`
+    font-size: ${props => props.theme.fontlg};
+    color: ${props => `rgba(${props.theme.bodyRgba},0.6)`};
+    align-self: flex-start;
+    margin: 1rem auto;
+    font-weight: 400;
+`
+const ButtonContainer = styled.div`
+    width: 80%;
+    align-self: flex-start;
+`
+
 function About() {
   return (
-    <Section>About</Section>
+    <Section>
+      <Container>
+      <Box><Carousel /></Box>
+      <Box>
+        <Title>Welcome To The Weirdos Club.</Title>
+        <SubText>The WEIRDOS CLUB is a private collection of NFTs—unique digital collectibles. The Weirdos are stored as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS.</SubText>
+        <SubTextLight>With more than 200+ hand drawn traits, each NFT is unique and comes with a membership to an exclusive group of successful investors. Join an ambitious ever-growing community with multiple benefits and utilities.</SubTextLight>
+        <ButtonContainer>
+          <ThemeProvider theme={dark}>
+            <Button text="JOIN OUR DISCORD" link="#"></Button>
+          </ThemeProvider>
+        </ButtonContainer>
+      </Box>
+      </Container>
+    </Section>
   )
 }
 
