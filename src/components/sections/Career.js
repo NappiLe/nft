29ddf1118e08883e537fe84fa4f7d834806f6e24
NgocLe <bitbucket@example.com>
2023-07-30 15:32:46 +0700

    modify button in Career component

--- src/components/sections/Career.js
+++ src/components/sections/Career.js
@@ -95,6 +95,10 @@ const Button = styled.button`
 ` 
 
 function Career() {
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
 
   return (
     <Section id="career">
@@ -106,7 +110,7 @@ function Career() {
             <Text>
             Lorem ipsum nt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Text>
-            <Button 
+            <Button onClick={() => openInNewTab('https://stackoverflow.com')}
             >Join us now</Button>
             </Box>
             <Image src={career} alt="career"/>
